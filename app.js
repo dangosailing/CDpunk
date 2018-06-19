9a3333ed550094d0a762f18764a288235efc4bce
Daniel Salin <daniel.salin@gmail.com>
2018-06-19 22:11:14 +0200

    added beer song script

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Hello');
+var singular = "bottle";
+var plural = "bottles";
+
+var count = 99;
+
+while(count > 0)
+{
+        
+    console.log(count + " " + plural + " of beer on the wall");
+    console.log(count + " " +  plural + " of beer");
+    console.log("take one down and pass it around");
+    console.log(count + " " + plural + " of beer on the wall");
+    count -= 1;
+    console.log("-----");
+
+    if(count == 1)
+    {
+        console.log(count + " " + singular + " of beer on the wall");
+        console.log(count + " " +  singular + " of beer");
+        console.log("take one down and pass it around");
+        console.log(count + " " + plural + " of beer on the wall");
+        count -= 1;
+        console.log("-----");
+        } 
+     
+        
+
+
+} 
